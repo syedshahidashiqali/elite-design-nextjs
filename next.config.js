18dc0cef3f7f5411947d7e6f4688e22ccd4467f5
Syed Shahid Ashiq Ali <syedshahid1215@gmail.com>
2022-04-22 10:05:13 +0500

    comlplete project

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    loader: "default",
-    path: "/_next/image",
-  },
 };
 
 module.exports = nextConfig;
